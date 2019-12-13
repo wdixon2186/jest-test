4358982042c6f83e7eba556699540d794064360c
William Dixon <Wdixon@Williams-iMac.local>
2019-12-13 16:56:59 -0700

    added decrement

--- test/src/App.js
+++ test/src/App.js
@@ -6,15 +6,20 @@ class App extends Component {
     super();
     this.state = { counter: 0 };
     this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
   }
   increment() {
     this.setState({ counter: this.state.counter + 1 });
+  }
+  decrement() {
+    this.setState({ counter: this.state.counter - 1 });
   }
   render() {
     return (
       <div data-test="component-app">
         <h1>The count is {this.state.counter}</h1>
         <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
       </div>
     );
   }
