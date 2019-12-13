789285db6d787e088e4db76d7bfae44aa9073e54
William Dixon <Wdixon@Williams-iMac.local>
2019-12-13 16:49:03 -0700

    added tests

--- test/src/App.js
+++ test/src/App.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { counter: 0 };
+    this.increment = this.increment.bind(this);
+  }
+  increment() {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+  render() {
+    return (
+      <div data-test="component-app">
+        <h1>The count is {this.state.counter}</h1>
+        <button onClick={this.increment}>+</button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- test/src/App.test.js
+++ test/src/App.test.js
@@ -2,12 +2,22 @@ import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
 import App from "./App";
+import { exportAllDeclaration } from "@babel/types";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 test("renders without crashing", () => {
   const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
 });
+
+test("renders increment button", () => {});
+
+test("renders counter display", () => {});
+
+test("counter starts at 0", () => {});
+test("clicking button increments counter", () => {});
 
 //testing goal number 1. easy to maintain. test for behavior not integration. there are many ways to get a result. test for the result.
 //testing goal 2, easy diagnosis of failing test. when a test fails it should be easy to see why.
